e750c3b3177b25d55b8e033b0abba132efc04731
EHOTbl <87080886+EHOTbl@users.noreply.github.com>
2023-02-02 19:17:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let myRegex = /s+/g; // Change this line
 let result = difficultSpelling.match(myRegex);
 
 console.log(result)
+
+
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+console.log(result)
